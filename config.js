783c0a444de9bbb63daa9d7571ff9ff9f8044fab
melissa :) <susunong@gmail.com>
2026-01-25 16:39:01 -0500

    new api config endpoint

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Configuration file for 215 Supper Club Passport
 // Replace the URL below with your Google Apps Script deployment URL
 
-const API_ENDPOINT = 'YOUR_GOOGLE_APPS_SCRIPT_URL_HERE';
+const API_ENDPOINT = 'https://script.google.com/macros/s/AKfycbyxgA7LlY9lzbd_D8sQGs4Ps9PSx5gSAR1jSkmNiBapLUa8j14y0DBBN7Uh0DOKTZeDKw/exec';
 
 // Example:
 // const API_ENDPOINT = 'https://script.google.com/macros/s/AKfycbwu2_wqOdhz6ShGPDhhFMIhMyHcB2fQyXuPMXoi4gU6w002lIv0xYCVDk7NU9mmz0u8bw/exec';
